2ed394dbdb51a6f10c123e38c4e029e91e7dbedc
thcarval <contatothalitanunes@gmail.com>
2022-01-27 10:03:52 -0300

    Update @ courses

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -12,9 +12,9 @@ export class CourseComponent implements OnInit {
   ngOnInit(): void {
   }
   course=[
-    {'id':1, 'name':'Aprenda X', 'description':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.', 'image':'../../assets/cursos.jpg'},
-    {'id':2, 'name':'Aprenda Z', 'description':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.', 'image':'../../assets/cursos.jpg'},
-    {'id':3, 'name':'Aprenda Y', 'description':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.', 'image':'../../assets/cursos.jpg'},
-    {'id':4, 'name':'Aprenda W', 'description':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.', 'image':'../../assets/cursos.jpg'}
+    {'id':1, 'name':'Aprenda JavaScript', 'description':'Curso gratuito com os primeiros passos de JavaScript', 'image':'../../assets/cursos.jpg', 'href':'https://www.youtube.com/watch?v=BXqUH86F-kA&list=PLntvgXM11X6pi7mW0O4ZmfUI1xDSIbmTm'},
+    {'id':2, 'name':'Full Stack JavaScript Profissional', 'description':'A forma que eu melhor aprendo é praticando. Esse curso é completão, com aulas, exercícios com resolução e projetos reais de forma totalmente prática, focado no mercado de trabalho. ', 'image':'../../assets/cursos.jpg', 'href':'https://go.hotmart.com/U65802408K'},
+    {'id':3, 'name':'Java para Web', 'description':'Com o ramo da programação em constante crescimento, a linguagem Java para Web é considerada uma das mais utilizadas e bem remuneradas.', 'image':'../../assets/cursos.jpg', 'href':'https://go.hotmart.com/P65820588O'},
+    {'id':4, 'name':'Angular', 'description':'Desenvolvi esse site usando Angular, que é um framework de JavaScript. Gosto muito da tecnologia, então separei esse curso para que você possa desenvolver sites como esse.', 'image':'../../assets/cursos.jpg', 'href':'https://go.hotmart.com/D65820799G'}
   ]
 }
